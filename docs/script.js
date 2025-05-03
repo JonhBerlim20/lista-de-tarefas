9c1cbc1671968410fcd2282e453750ef9b0f474b
JonhBerlim20 <joao.palmieri0900@gmail.com>
2025-05-02 22:41:03 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,9 @@
-function mostrarMensagem(mensagem, texto)
+const mensagem = document.getElementById("mensagem");
+let tarefas = [];
+function mostrarMensagem(texto, cor)
 {
     mensagem.textContent = texto;
+    mensagem.style.color = cor;
     mensagem.classList.add("transicao");
 
     setTimeout (() => 
@@ -11,29 +14,45 @@ function mostrarMensagem(mensagem, texto)
         {
             mensagem.textContent = "";
         }, 1000);
-    }, 3000); 
+    }, 4000); 
 }
 
 function adicionarTarefa() {
     
-    let mensagem = document.getElementById("mensagem");
-    let inputTarefa = document.getElementById("inputTarefa"); //A variavel inputTarefa armazena a linha do input, ou seja, a caixa de texto, não o valor digitado nela.
-    let tarefa = inputTarefa.value; //A variavel tarefa armazena o valor digitado pelo usuario de dentro da caixa de texto devido o uso do .value
+    const inputTarefa = document.getElementById("inputTarefa"); //A variavel inputTarefa armazena a linha do input, ou seja, a caixa de texto, não o valor digitado nela.
+    let tarefa = inputTarefa.value.trim(); //A variavel tarefa armazena o valor digitado pelo usuario de dentro da caixa de texto devido o uso do .value
 
-    if (tarefa != "")
+    if (tarefas.includes(tarefa)) 
     {
-        let listaTarefa = document.getElementById("listaTarefa"); //listaTarefa armazena a lista ul
+        mostrarMensagem("Esta tarefa já está registrada!", "#ea0000");
+    } 
+    else 
+    {
+        if (tarefa != "")
+        {
+            mostrarMensagem("Tarefa adicionada com sucesso!", "#008000");
+            tarefas.push(tarefa);
+            atualizarTarefas();
+        }
+        else
+        {
+            mostrarMensagem("O campo está vazio. Digite uma tarefa para continuar!", "#ea0000");
+        }
+    }
+
+    inputTarefa.value = ""; // A informação da caixa é esvaziada.
+}
+
+function atualizarTarefas() {
+    const listaTarefa = document.getElementById("listaTarefa"); //listaTarefa armazena a lista ul
+    listaTarefa.innerHTML = "";
+
+    for (i in tarefas) {
         let novaTarefa = document.createElement("li"); //novaTarefa cria uma novo campo vazio
-        mostrarMensagem(mensagem , "Tarefa adicionada com sucesso!");
-
-        novaTarefa.textContent = tarefa; //Adicionamos em novaTarefa uma informação, agora o campo possui um dado.
+        novaTarefa.textContent = tarefas[i]; //Adicionamos em novaTarefa uma informação, agora o campo possui um dado.
         listaTarefa.appendChild(novaTarefa); //Adicionamos na lista o campo de novaTarefa, atualizando a lista e mostrando no monitor.
-        inputTarefa.value = ""; // A informação da caixa é esvaziada.
     }
-    else
-    {
-        mostrarMensagem(mensagem , "O campo está vazio. Digite uma tarefa para continuar");
-    }
+    
 }
 
 window.onload = () => {
