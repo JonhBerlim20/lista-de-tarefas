054d6d4383c1c7d24c594a2ae2bcf0127c132854
JonhBerlim20 <joao.palmieri0900@gmail.com>
2025-09-18 21:22:27 -0300

    Delete docs directory

--- docs/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(e) {
-    console.log('Service Worker instalado');
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
-  });
-  
